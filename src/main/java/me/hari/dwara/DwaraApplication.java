777c677743abef266c57f122ef83e3edce5f35e7
hari-hara-1 <hari.shenbagan@gmail.com>
2026-08-07 12:56:08 +0530

    Initial commit

---
+++ src/main/java/me/hari/dwara/DwaraApplication.java
@@ -0,0 +1,13 @@
+package me.hari.dwara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hari/dwara/DwaraApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hari.dwara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
